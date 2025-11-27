c1ec744805c41a67c9a61a25355777084203e802
Patryk <pat20036@gmail.com>
2025-11-27 13:26:32 +0100

    refactor padding extension

--- ui-components/src/main/java/com/pat/equalizer/modifiers/PaddingExtensions.kt
+++ ui-components/src/main/java/com/pat/equalizer/modifiers/PaddingExtensions.kt
@@ -4,6 +4,4 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 
-fun Modifier.defaultHorizontalPadding() = this.then(
-    Modifier.padding(horizontal = 16.dp)
-)
+fun Modifier.defaultHorizontalPadding() = this.padding(horizontal = 16.dp)
